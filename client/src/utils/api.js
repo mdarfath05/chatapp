7be9f79a83de3b05bc5085e44ccf10450e81a83d
mdarfath05 <167604370+mdarfath05@users.noreply.github.com>
2026-02-20 18:42:13 +0530

    Update API base URL to use environment variable

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:5001/api',
+  const api = axios.create({
+  baseURL: process.env.REACT_APP_SERVER_URL
+    ? `${process.env.REACT_APP_SERVER_URL}/api`
+    : '/api',
   headers: { 'Content-Type': 'application/json' },
 });
 
